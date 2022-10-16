4f416d3be05acce49f132f5f565f896feaaa49af
JoJo <l19priv@gmail.com>
2022-10-16 18:05:30 -0400

    footer colors, and alignment

--- 02_Mignonne_Charity/src/routes/components/shared/Footer.svelte
+++ 02_Mignonne_Charity/src/routes/components/shared/Footer.svelte
@@ -23,12 +23,12 @@
       <div class="company-dieu-peu-top-wrapper">
         <div class="company-dieu-peu-lower-wrapper">
           <div class="company-dieu-peu-title">
-            <span class="company-dieu-peu-title-text">Dieu Peut</span>
+            <span class="company-dieu-peu-title-text">Dieu Peut Charity</span>
           </div>
           <div class="company-dieu-peu-social-media">
             <div class="buttons-social-media-top-wrapper">
               <div class="buttons-social-media-lower-wrapper">
-                <a href="https://www.tiktok.com">
+                <a href="https://www.tiktok.com" target="_blank">
                   <div class="social-media-buttons" id="social-media-button-tiktok">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="50%" height="auto" viewBox="0 0 256 256" xml:space="preserve">
                     <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
@@ -37,7 +37,7 @@
                     </svg>
                   </div>
                 </a>
-                <a href="https://www.facebook.com">
+                <a href="https://www.facebook.com" target="_blank">
                   <div class="social-media-buttons" id="social-media-button-facebook">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" width="70%" height="auto">
                     <g id="surface32576398">
@@ -46,21 +46,28 @@
                     </svg>
                   </div>
                 </a>
-                <a href="https://www.twitter.com">
+                <a href="https://www.twitter.com" target="_blank">
                   <div class="social-media-buttons" id="social-media-button-twitter">
                     <svg fill="#00000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 16 16" width="56%" height="auto"><path d="M 15 3.296875 C 14.476563 3.523438 13.949219 3.691406 13.367188 3.746094 C 13.949219 3.410156 14.417969 2.84375 14.648438 2.226563 C 14.066406 2.5625 13.484375 2.789063 12.84375 2.902344 C 12.257813 2.339844 11.5 2 10.683594 2 C 9.109375 2 7.824219 3.242188 7.824219 4.765625 C 7.824219 4.988281 7.824219 5.214844 7.882813 5.386719 C 4.875 5.386719 2.8125 3.691406 1.414063 2 C 1.121094 2.394531 1.003906 2.902344 1.003906 3.410156 C 1.003906 4.367188 1.53125 5.214844 2.289063 5.722656 C 1.820313 5.667969 1.355469 5.554688 1.003906 5.386719 C 1.003906 5.386719 1.003906 5.386719 1.003906 5.441406 C 1.003906 6.796875 1.996094 7.921875 3.28125 8.148438 C 3.046875 8.203125 2.8125 8.261719 2.519531 8.261719 C 2.347656 8.261719 2.171875 8.261719 1.996094 8.207031 C 2.347656 9.335938 3.976563 10.632813 5.257813 10.632813 C 4.265625 11.363281 3.34375 12 1.5 12 C 1.265625 12 1.453125 12 1 12 C 2.28125 12.789063 3.800781 13 5.375 13 C 10.683594 13 13.542969 8.769531 13.542969 5.101563 C 13.542969 4.988281 13.542969 4.878906 13.542969 4.765625 C 14.125 4.367188 14.59375 3.863281 15 3.296875"/>
                     </svg>
                   </div>
                 </a>
-                <a href="https://www.instagram.com">
+                <a href="https://www.instagram.com" target="_blank">
                   <div class="social-media-buttons" id="social-media-button-instagram">
                     <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 16 16" width="57%" height="auto"><path d="M 4.773438 1 C 2.695313 1 1 2.695313 1 4.773438 L 1 10.230469 C 1 12.304688 2.695313 14 4.773438 14 L 10.230469 14 C 12.304688 14 14 12.304688 14 10.226563 L 14 4.773438 C 14 2.695313 12.304688 1 10.226563 1 Z M 4.773438 2 L 10.226563 2 C 11.765625 2 13 3.234375 13 4.773438 L 13 10.226563 C 13 11.765625 11.765625 13 10.230469 13 L 4.773438 13 C 3.234375 13 2 11.765625 2 10.230469 L 2 4.773438 C 2 3.234375 3.234375 2 4.773438 2 Z M 11.5 3 C 11.222656 3 11 3.222656 11 3.5 C 11 3.777344 11.222656 4 11.5 4 C 11.777344 4 12 3.777344 12 3.5 C 12 3.222656 11.777344 3 11.5 3 Z M 7.5 4 C 5.574219 4 4 5.574219 4 7.5 C 4 9.425781 5.574219 11 7.5 11 C 9.425781 11 11 9.425781 11 7.5 C 11 5.574219 9.425781 4 7.5 4 Z M 7.5 5 C 8.886719 5 10 6.113281 10 7.5 C 10 8.886719 8.886719 10 7.5 10 C 6.113281 10 5 8.886719 5 7.5 C 5 6.113281 6.113281 5 7.5 5 Z"/>
                     </svg>
                   </div>
                 </a>
-                <a href="https://www.youtube.com">
+                <a href="https://www.youtube.com" target="_blank">
                   <div class="social-media-buttons" id="social-media-button-youtube">
                     <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 16 16" width="58%" height="auto"><path d="M 2.488281 3.011719 C 1.664063 3.011719 0.988281 3.6875 0.988281 4.511719 L 0.988281 11.511719 C 0.988281 12.335938 1.664063 13.011719 2.488281 13.011719 L 13.488281 13.011719 C 14.308594 13.011719 14.988281 12.335938 14.988281 11.511719 L 14.988281 4.511719 C 14.988281 3.6875 14.3125 3.011719 13.488281 3.011719 Z M 2.488281 4.011719 L 13.488281 4.011719 C 13.769531 4.011719 13.988281 4.230469 13.988281 4.511719 L 13.988281 11.511719 C 13.988281 11.792969 13.769531 12.011719 13.488281 12.011719 L 2.488281 12.011719 C 2.207031 12.011719 1.988281 11.792969 1.988281 11.511719 L 1.988281 4.511719 C 1.988281 4.230469 2.207031 4.011719 2.488281 4.011719 Z M 6 4.992188 L 6 11.011719 L 11 8 Z M 7 6.757813 L 9.0625 8 L 7 9.242188 Z"/>
+                    </svg>
+                  </div>
+                </a>
+                <a href="https://www.nerlont.com" target="_blank">
+                  <div class="social-media-buttons" id="social-media-button-nerlont">
+										<svg width="40" height="auto" viewBox="0 0 83 83" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M27.0035 30.6939L24.7186 30.7525C24.5624 30.5572 24.4842 30.3033 24.4842 29.9908C24.4842 29.6784 24.5233 29.4049 24.6014 29.1706C27.2574 28.8191 29.4252 28.2722 31.1047 27.5301C31.6515 27.2958 31.9835 27.1786 32.1007 27.1786C32.4522 27.1786 32.8819 27.7254 33.3897 28.8191C33.8974 29.9127 34.3271 31.1235 34.6786 32.4515C38.6235 30.2252 42.4708 28.4675 46.2205 27.1786C48.6031 27.1786 50.4388 28.1355 51.7278 30.0494C53.0167 31.9242 53.6612 34.424 53.6612 37.5487L53.544 45.2824C53.544 51.8052 53.8369 55.1252 54.4228 55.2424C55.0087 55.3205 56.1024 55.3595 57.7038 55.3595C57.86 55.6329 57.9381 55.9259 57.9381 56.2384C57.9381 56.5508 57.86 56.8633 57.7038 57.1758L52.3137 57C49.0718 57 46.4939 57.1562 44.58 57.4687C44.3847 57.2344 44.2871 56.9024 44.2871 56.4727L44.3457 56.004C45.4784 55.7306 46.3572 55.3595 46.9821 54.8908C47.6071 54.3831 47.9977 53.9144 48.1539 53.4847C48.3492 53.016 48.5054 52.3325 48.6226 51.4341C48.7398 50.4967 48.7984 48.7 48.7984 46.044V41.1812C48.7984 38.4471 48.4859 36.1426 47.8609 34.2678C47.5875 33.5256 47.2751 32.9788 46.9235 32.6273C46.0642 31.6899 45.0292 31.2212 43.8184 31.2212C42.6075 31.2212 41.3967 31.4555 40.1859 31.9242C38.9751 32.3929 38.0572 32.8226 37.4322 33.2132C36.8073 33.6038 35.9285 34.1896 34.7958 34.9708C34.6395 38.0565 34.5614 42.1967 34.5614 47.3915C34.5614 52.5473 34.8153 55.1642 35.3231 55.2424C35.8699 55.3205 36.8659 55.3595 38.3111 55.3595C38.4673 55.6329 38.5454 55.9259 38.5454 56.2384C38.5454 56.5508 38.4673 56.8633 38.3111 57.1758L33.3311 57C30.3626 57 27.7847 57.1562 25.5974 57.4687C25.4021 57.2344 25.3045 56.9024 25.3045 56.4727L25.3631 56.004C26.4958 55.7306 27.3746 55.3595 27.9995 54.8908C28.6245 54.3831 29.0151 53.9144 29.1713 53.4847C29.3666 53.016 29.5228 52.1762 29.64 50.9654C29.7962 49.7155 29.8744 47.0595 29.8744 42.9974C29.8744 38.9353 29.8548 36.6894 29.8158 36.2598C29.8158 35.7911 29.7962 35.2052 29.7572 34.5021C29.7181 33.7991 29.64 33.2718 29.5228 32.9202C29.4057 32.5687 29.2299 32.1976 28.9955 31.8071C28.6049 31.0649 27.9409 30.6939 27.0035 30.6939Z" fill="black"/>
                     </svg>
                   </div>
                 </a>
@@ -75,8 +82,8 @@
 								<div class="company-dieu-peu-address-1-address" id="address-text"> 
 									Dieu Peut Charity <br />
 									123 Temporary Ave <br />
-									Toronto, Ontario <br />
-									Canada, M6T 2SR <br />
+									Toronto, Ontario, M6T 2SR <br />
+									Canada<br />
 								</div>
 							</div>
 						</div>
@@ -87,8 +94,8 @@
 								<div class="company-dieu-peu-address-2-address" id="address-text"> 
 									Haitian office: <br />
 									321 Blvd. Faux Chemin, <br />
-									Port-au-Prince, Ouest<br />
-									Haiti, 6221 <br />
+									Port-au-Prince, Ouest, 6221<br />
+									Haiti<br />
 								</div>
 							</div>
 						</div>
@@ -115,21 +122,15 @@
 			</div>
       <div class="company-l19579-wrapper">
         <div class="company-l19579-description">
-						<div class="managed-by-statement">
+						<a id="managed-by-statement-link" href="https://www.l19579.com" target="_blank">
 							Written, and managed by&nbsp
-							<a id="managed-by-statement-link" href="https://www.l19579.com" target="_blank">
-								<img id="l19579-logo" src="https://avatars.githubusercontent.com/u/98875234?v=4" alt="L19579" /> L19579 <br />
-							</a>
-						</div>
+							<img id="l19579-logo" src="https://avatars.githubusercontent.com/u/98875234?v=4" alt="L19579" /> L19579 <br />
+						</a>
 					</div>
 					<div class="l19579-mit-license-wrapper">
-						<div class="l19579-mit-license-text">
+						<a class="l19579-mit-license-text" href="https://github.com/L19579/L19_Cyrus/tree/main/02_Mignonne_Charity" target="_blank">
 							Apache-2.0 / MIT license &nbsp
-						</div>
-						<div id="social-media-button-github">
-							<svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 64 64" width="17px" height="auto"><path d="M32 6C17.641 6 6 17.641 6 32c0 12.277 8.512 22.56 19.955 25.286-.592-.141-1.179-.299-1.755-.479V50.85c0 0-.975.325-2.275.325-3.637 0-5.148-3.245-5.525-4.875-.229-.993-.827-1.934-1.469-2.509-.767-.684-1.126-.686-1.131-.92-.01-.491.658-.471.975-.471 1.625 0 2.857 1.729 3.429 2.623 1.417 2.207 2.938 2.577 3.721 2.577.975 0 1.817-.146 2.397-.426.268-1.888 1.108-3.57 2.478-4.774-6.097-1.219-10.4-4.716-10.4-10.4 0-2.928 1.175-5.619 3.133-7.792C19.333 23.641 19 22.494 19 20.625c0-1.235.086-2.751.65-4.225 0 0 3.708.026 7.205 3.338C28.469 19.268 30.196 19 32 19s3.531.268 5.145.738c3.497-3.312 7.205-3.338 7.205-3.338.567 1.474.65 2.99.65 4.225 0 2.015-.268 3.19-.432 3.697C46.466 26.475 47.6 29.124 47.6 32c0 5.684-4.303 9.181-10.4 10.4 1.628 1.43 2.6 3.513 2.6 5.85v8.557c-.576.181-1.162.338-1.755.479C49.488 54.56 58 44.277 58 32 58 17.641 46.359 6 32 6zM33.813 57.93C33.214 57.972 32.61 58 32 58 32.61 58 33.213 57.971 33.813 57.93zM37.786 57.346c-1.164.265-2.357.451-3.575.554C35.429 57.797 36.622 57.61 37.786 57.346zM32 58c-.61 0-1.214-.028-1.813-.07C30.787 57.971 31.39 58 32 58zM29.788 57.9c-1.217-.103-2.411-.289-3.574-.554C27.378 57.61 28.571 57.797 29.788 57.9z"/>
-							</svg>
-						</div>
+						</a>
 					</div>
       </div>
     </div>
@@ -221,29 +222,30 @@
       flex
       justify-center
       bg-stone-700
-			pt-10
+			pt-12
+			pb-5
     ;
     height: 200px;
     width: 100%;
-    border: 2px solid red;
+    /* border: 2px solid red; */
   }
 
   .company-lower-panel-wrapper{
     @apply
-			text-stone-400
+			text-stone-300
     ;
     display: grid;
     grid-template-columns: repeat(10, [col] 10%);
     grid-template-rows: minmax(0, 1fr) repeat(5, [row] 20%);
-    height: 100%;
-    width: 1100px;
-    border: 2px solid red;
+    height: 90%;
+    width: 1200px;
+    /* border: 2px solid red; */
   }
   
   .company-dieu-peu-top-wrapper{
     grid-column: col / span 7;
     grid-row: row / span 5;
-    border: 2px solid pink;
+   /* border: 2px solid pink; */
   }
   
   .company-dieu-peu-lower-wrapper{
@@ -251,8 +253,8 @@
     display: grid;
     grid-template-columns: repeat(10, [col] 10%);
     grid-template-rows: repeat(4, [row] 25%);
-    border: 2px solid blue;
     height: 99%; /* <-- minor overlap at 100%, gap somewhere? */
+    /* border: 2px solid blue; */
   }
 
   .company-dieu-peu-title{
@@ -266,12 +268,13 @@
     grid-column: col / span 4;
     grid-row: row;
     white-space: nowrap;
-    border: 2px solid white;
+    /* border: 2px solid white; */
   }
 
 	.company-dieu-peu-title-text{
 		transition: color 0.5s;
 	}
+
 	.company-dieu-peu-title-text:hover{
 		@apply
 			text-stone-100
@@ -279,49 +282,53 @@
 	}
   
   .company-dieu-peu-social-media{
-    overflow: hidden;
     grid-column: col 5 / span 6;
     grid-row: row;
-    border: 2px solid purple;
+    /* border: 2px solid purple; */
   }
   
   .buttons-social-media-top-wrapper{
     @apply
       flex
       justify-center
-      pl-9
+      pl-8
     ;
     height: 90%; /* This is producing minor misalignment @ 100%, no margins */
-    border: 2px solid yellow;
+    /* border: 2px solid yellow; */
   }
   
   .buttons-social-media-lower-wrapper{
     @apply
       flex
-      grid grid-cols-5 gap-4
+      grid grid-cols-6 gap-5
     ;
-    width: 230px;
+    width: 300px;
     height: 100%;
-    border: 2px solid red;
+    /* border: 2px solid red; */
   }
+
+	#social-media-button-facebook{
+			padding-left: 1.4px;
+	}
   
   .social-media-buttons{
     @apply
       flex
       flex-col
       items-center
-      bg-stone-500
+      bg-stone-400
       rounded-full
     ;
     height: 32px;
     width: 32px;
-    border: 0.1px solid black;
+    border: 0.01px solid black;
   }
   
   .social-media-buttons:hover{
 		@apply
       bg-stone-300
 		;
+		overflow: visible;
     transition-duration: 200ms;
     transform:scale(1.1);
     transition-timing-function: cubic-bezier(0.4, 0,)
@@ -329,23 +336,23 @@
   
   /* @@@@@@@ */
 		.company-dieu-peu-address-all-wrapper{
-			padding-top: 1rem;
+			padding-top: 2rem;
 			grid-column: col 1 / span 10;
 			grid-row: row 2 / span 4;
 			
 			display: grid;	
-      grid-template-columns: repeat(9, [col] 9%);
+      grid-template-columns: repeat(30, [col] 1%);
       grid-template-rows: repeat(5, [row] 20%);
-      border: 2px solid pink;
+      /* border: 2px solid pink; */
 			
 		}
 
     .company-dieu-peu-address-top-wrapper-1{
       overflow: hidden;
-      grid-column: col 1 / span 3;
+      grid-column: col 3 / span 26;
       grid-row: row 1 / span 5;
 
-      border: 2px solid green;
+      /* border: 2px solid green; */
     } 
 
     .company-dieu-peu-address-lower-wrapper-1{
@@ -354,20 +361,20 @@
       overflow: hidden;
       grid-template-columns: repeat(5, [col] 20%);
       grid-template-rows: repeat(5, [row] 20%);
-      border: 2px solid black;
+      /* border: 2px solid black; */
     }
 
     .company-dieu-peu-address-1-address{
       grid-column: col 1 / span 5;
       grid-row: row 1 / span 5;
-      border: 2px solid white;
+      /* border: 2px solid white; */
     }
     
     .company-dieu-peu-address-top-wrapper-2{
       overflow: hidden;
-      grid-column: col 4 / span 3;
+      grid-column: col 32 / span 25;
       grid-row: row 1 / span 5;
-      border: 2px solid green;
+     /* border: 2px solid green; */
     } 
     
   .company-dieu-peu-address-lower-wrapper-2{
@@ -376,17 +383,18 @@
       overflow: hidden;
       grid-template-rows: repeat(5, [row] 20%);
       grid-template-columns: repeat(5, [col] 20%);
-      border: 2px solid black;
+      /* border: 2px solid black; */
     }
 
     .company-dieu-peu-address-2-address{
       grid-column: col 1 / span 5;
       grid-row: row 1 / span 5;
-      border: 2px solid white;
+      /* border: 2px solid white; */
     }
 
     #address-text{
-			font-weight: 300;
+			letter-spacing: 0.015rem;
+			font-weight: 400;
       position: relative;
       padding-left: 1rem;
 			line-height: 1.4rem;
@@ -394,9 +402,9 @@
 
     .company-dieu-peu-address-top-wrapper-3{
       overflow: hidden;
-      grid-column: col 7 / span 4;
+      grid-column: col 58 / span 30;
       grid-row: row 1 / span 5;
-      border: 2px solid green;
+      /* border: 2px solid green; */
     } 
     
   .company-dieu-peu-address-lower-wrapper-3{
@@ -405,7 +413,7 @@
       overflow: hidden;
       grid-template-rows: repeat(5, [row] 20%);
       grid-template-columns: repeat(5, [col] 20%);
-      border: 2px solid black;
+      /* border: 2px solid black; */
     }
     
     /* Issues w/ grid selections here. Suspect inherited attr.
@@ -415,7 +423,7 @@
     .company-dieu-peu-address-3-address{
       grid-column: col 1 / span 4;
       grid-row: row 1 / span 5;
-      border: 2px solid white;
+      /* border: 2px solid white; */
     }
 
     #address-text{
@@ -428,26 +436,30 @@
   /* @@@@@@@ */
 
 	.company-dieu-peu-copyright-wrapper{
+		padding-right: 1.8rem;
     overflow: hidden;
     grid-column: col 8 / span 3;
     grid-row: row 1 / span 2;
-    border: 2px solid yellow;
 		height: 100%;
+    /* border: 2px solid yellow; */
 
 	}
 	
 	.company-dieu-peu-copyright-text{
-		padding-top: 1.7rem;
+		padding-top: 0.7rem;
 		text-align: right;
 	}
 
   /* -------------------- */
 
   .company-l19579-wrapper{
+		line-height: 1.4rem;
+		padding-right: 1.8rem;
     overflow: hidden;
     grid-column: col 8 / span 3;
-    grid-row: row 4 / span 2;
-    border: 2px solid yellow;
+    grid-row: row 3 / span 3;
+    /* border: 2px solid yellow; */
+		padding-top: 0.7rem;
 	
     text-align: right;
   }
@@ -456,22 +468,28 @@
     @apply
 			flex
 			flex-col
-      text-xl
+			pt-4
       px-3
 			justify-end
     ;
     white-space: nowrap; 
-		border: 3px solid green;
+		/* border: 3px solid green; */
   }
 	
-	a:link a:visited a:active a:hover{
-    text-decoration: none;
+	#managed-by-statement-link{
+		@apply
+			text-stone-300
+		;
+		transition: color 0.5s;
+		text-decoration: none;	
 	}
-
-	#managed-by-statement-link:link{
-    text-decoration: none;
-	}
-
+	
+	#managed-by-statement-link:hover{
+		@apply
+			text-stone-100
+		;
+	} 
+	
   #l19579-logo{
     @apply
       rounded-full
@@ -483,13 +501,24 @@
 	.l19579-mit-license-wrapper{
 		@apply
 			flex
-			align-middle
 			justify-end
 		;
-		border: 2px solid blue;
+		/* border: 2px solid blue; */
 	}
 	
 	.l19579-mit-license-text{
+		@apply
+			flex
+			text-stone-300
+		;
+		transition: color 0.5s;
+		text-decoration: none;
+	}
+	
+	.l19579-mit-license-text:hover{
+		@apply
+			text-stone-100
+		;
 	}
 		
 	#social-media-button-github{
@@ -497,6 +526,6 @@
 			pl-1
 			rounded-full
 		;
-		border: 2px solid red;	
+		/* border: 2px solid red; */	
 	}
 </style>
