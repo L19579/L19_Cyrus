e6d07b4f43496b4072fd2a425a56b0e1d9417e61
JoJo <l19priv@gmail.com>
2022-11-24 23:27:31 -0500

    adapter update; test pg

---
+++ 02_Mignonne_Charity/src/routes/testlivepayment/+page.svelte
@@ -0,0 +1,67 @@
+<script>
+	import { writable } from 'svelte/store';
+	
+	let foo = 'baz'
+	let bar = 'qux'
+	let result = null
+	
+	
+	export const user = writable ({
+	first_name: "Gigi",
+	last_name: "Fifi",
+	email: "Gigi@fifi.com",
+	phone: "1112223344",
+	address: {
+		line1: "416 Yadi Road",
+		line2: "",
+		city: "Boogieville",
+		province: "Manitoba",
+		postal_code: "K1H 2R4",
+		country: "Canaada",
+	},
+	card: {
+		amount: 6666,
+		number: "4242424242424242",
+		cvc: "123",
+		exp_month: 2,
+		exp_year: 24,
+	},
+})
+
+async function doyathingtwentyone () {
+		const res = await fetch('http://172.105.104.235:8000/doyathingtwentyone', {
+			method: 'POST',
+		body: JSON.stringify({user})
+		})
+		
+		const json = await res.json()
+		result = JSON.stringify(json)
+	}
+	
+	
+	async function doPost () {
+		const res = await fetch('https://httpbin.org/post', {
+			method: 'POST',
+			body: JSON.stringify({
+				foo,
+				bar
+			})
+		})
+		
+		const json = await res.json()
+		result = JSON.stringify(json)
+	}
+</script>
+
+
+<input bind:value={foo} />
+<input bind:value={bar} />
+<button type="button" on:click={doyathingtwentyone}>
+	Post it.
+</button>
+<p>
+	Result:
+</p>
+<pre>
+{result}
+</pre>
