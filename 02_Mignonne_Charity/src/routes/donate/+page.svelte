27660d758d08ef6842f6733b671759490d0a96c6
JoJo <l19priv@gmail.com>
2022-12-11 23:16:12 -0500

    donate cont'd

--- 02_Mignonne_Charity/src/routes/donate/+page.svelte
+++ 02_Mignonne_Charity/src/routes/donate/+page.svelte
@@ -9,36 +9,127 @@
     name: "",
     email: "",
     amount: 0,
-    card_number: "",
+    card_number: "10",
     exp: "",
     cvc: "",
-    postal_code: "",
     country: "",
+    postal_code: ""
   });
 
-  let first_option_color = 'blue';
-  let second_option_color = 'blue';
-  let third_option_color = 'blue';
+  function into_readable(mod_id){
+    console.log("DEBUG - into_readable: start");
+    switch(mod_id){
+      case 0: // card_number
+        switch(payment_form.card_number.length){
+          case(4):
+          case(9):
+          case(14):
+            payment_form.card_number = payment_form.card_number + " ";
+            break;
+        };
+        break;
+      case 1:  // exp
+        switch(payment_form.exp.length){
+          case(2):
+            payment_form.exp = payment_form.exp + " / "; 
+            break;
+        }
+        break;
+      case 2:  // postal_code
+        switch(payment_form.country){
+          case "canada":
+            if (payment_form.postal_code.length == 3){
+              payment_form.postal_code = payment_form.postal_code + " ";
+            };
+          case "haiti":
+            if (payment_form.postal_code.length == 2){
+              payment_form.postal_code = payment_form.postal_code + " ";
+            };
+            break;
+          default:
+            break;
+        }
+        break;
+    };
+  }  
 
-  function handle_click_release(is_down, option_id, donation_amount){
-    console.log("got a click");
+  function card_number_remove_spaces(){
+    // TODO  
+  }
+
+  let colors = [
+    'rgba(66,245,185,1)',
+    'rgba(50,168,129,1)',
+    'rgba(73,234,242,1)',
+    'rgba(46,152,158,1)',
+    'rgba(235,89,128,1)',
+    'rgba(179,61,92,1)'
+  ];
+
+  let first_option_color = colors[0];
+  let second_option_color = colors[2];
+  let third_option_color = colors[4];
+
+  function handle_click_release(is_down, option_id){
+    console.log("got a click/release");
+    let donation_amount = 0;
     switch(option_id){
       case 1:
-        first_option_color = first_option_color == 'blue' ? 'yellow' : 'blue'; 
+        donation_amount = 5;
+        first_option_color = !is_down ? colors[0] : colors[1]; 
+        break;
       case 2:
-        second_option_color = second_option_color == 'blue' ? 'yellow' : 'blue'; 
+        donation_amount = 10;
+        second_option_color = !is_down ? colors[2] : colors[3]; 
+        break;
       case 3:
-        third_option_color = third_option_color == 'blue' ? 'yellow' : 'blue'; 
+        donation_amount = 15;
+        third_option_color = !is_down ? colors[4] : colors[5]; 
+        break;
     }
     payment_form.amount = is_down ? donation_amount : payment_form.amount; 
     
     console.log("DEBUG: variables: " + is_down + " " + option_id + " " 
         + donation_amount + " " + payment_form.amount + ".");
   };
-
-  function handleRelease(){
+  
+  function test_country(){
+    console.log("Country: " + payment_form.country);
+  }
+  
+  function handleRelease(){ // Test
     test_bg = 'white'
   }
+  
+  let postal_code_placeholder = "A1A 2B2";
+  let postal_code_max_length = 7;
+
+  function format_postal_code(){
+    payment_form.postal_code = ""; 
+    switch(payment_form.country){
+      case "canada":
+        postal_code_max_length = 7;
+        postal_code_placeholder = "A1A 1A1";
+        break;
+      case "dominican republic":
+        postal_code_max_length = 5;
+        postal_code_placeholder = "12345";
+        break;
+      case "france":
+        postal_code_max_length = 5;
+        postal_code_placeholder = "12345";
+        break;
+      case "haiti":
+        postal_code_max_length = 7;
+        postal_code_placeholder = "HT 1234";
+        break;
+      case "united states":
+        postal_code_max_length = 5;
+        postal_code_placeholder = "12345";
+        break;
+    }; 
+  }
+
 </script>
 
 <Navbar current_nav_page = 'donate'/>
@@ -47,26 +138,33 @@
     <div class="donate-inner-wrapper">
 
       <div class="donate-thank-you-panel">
-        <div id="group-thankyou-subsection">
-          <div class="group-form-input-wrapper">
-            <input class="group-half-input-fields" type="text" placeholder="100" bind:value={payment_form.amount} />
+        <div id="group-thank-you-subsection">
+
+          <div id="amount-manual-wrapper">
+            <input class="group-full-input-fields" id="amount-manual-field" type="text" 
+              placeholder="$ 0" bind:value={payment_form.amount} />
             <br />
-            <label> Amount </label>
+            <div id="amount-manual-field-text"> Amount </div>
+          </div> 
+
+          <div class="amount-options-wrapper">
            
             <div class="group-amount-options" id="first-amount-option"
+              on:mousedown={() => handle_click_release(1, 1)} on:mouseup={() => handle_click_release(0, 1)} 
               style="--first-option-color: {first_option_color}">  
               <div class="group-amount-option-text" id="first-amount-option-text">
                 $5
               </div>
             </div>  
             <div class="group-amount-options" id="second-amount-option" 
+              on:mousedown={() => handle_click_release(1, 2)} on:mouseup={() => handle_click_release(0, 2)} 
               style="--second-option-color: {second_option_color}">  
               <div class="group-amount-option-text" id="second-amount-option-text">
                 $10
               </div>
             </div>  
             <div class="group-amount-options" id="third-amount-option"
-              on:mousedown={() => handle_click_release(1, 3, 15)} on:mouseup={() => handle_click_release(0, 3, 15)} 
+              on:mousedown={() => handle_click_release(1, 3)} on:mouseup={() => handle_click_release(0, 3)} 
               style="--third-option-color: {third_option_color}">  
               <div class="group-amount-option-text" id="third-amount-option-text">
                 $15
@@ -120,11 +218,13 @@
               <label>Card Information</label>
               <div class="group-form-input-wrapper" id="card-form-input-wrapper">
                 <input class="group-full-input-fields" id="card-number-field" type="text"
-                  placeholder="1234 1234 1234 1234" bind:value={$payment_form.card_number} />
+                  bind:value={payment_form.card_number} on:input={() => into_readable(0)}
+                  placeholder="1234 1234 1234 1234" maxlength="19"/>
                 <input class="group-full-input-fields" id="card-exp-field" type="text"
-                  placeholder="MM / YY" bind:value={$payment_form.exp} />
+                  on:input={() => into_readable(1)} bind:value={payment_form.exp} 
+                  placeholder="MM / YY" maxlength="7"/>
                 <input class="group-full-input-fields" id="card-cvc-field" type="text"
-                  placeholder="CVC" bind:value={$payment_form.cvc} />
+                  bind:value={payment_form.cvc} placeholder="CVC" maxlength="3" />
               </div>
             </div>
 
@@ -140,10 +240,17 @@
             <div class="group-form-subsection">
               <label> Country / Region </label>
               <div class="group-form-input-wrapper" id="region-form-input-wrapper">
-                <input class="group-full-input-fields" id="region-country-field" type="text"
-                  placeholder="Country" bind:value={$payment_form.country} />
+                <select class="group-full-input-fields" id="region-country-field" type="text"
+                  bind:value={payment_form.country} on:change={format_postal_code}> 
+                  <option value="canada" selected="selected">Canada</option>
+                  <option value="dominican republic" selected="selected">Dominican Republic</option>
+                  <option value="france" selected="selected">France</option>
+                  <option value="haiti" selected="selected">Haiti</option>
+                  <option value="united states" selected="selected">United States</option>
+                </select>
                 <input class="group-full-input-fields" id="region-postal-code-field" type="text"
-                  placeholder="Postal Code" bind:value={$payment_form.postal_code} />
+                  bind:value={payment_form.postal_code} on:input={() => into_readable(2)}
+                  placeholder={postal_code_placeholder} maxlength={postal_code_max_length} />
               </div>
             </div>
 
@@ -191,6 +298,8 @@
   /* ---------------- Thank You Section ---------------- */
 
   .test{ /* Delete */
+    display: flex;
+    justify-content: center;
     position: relative;
     width: 100%;
     font-size: 50px;
@@ -198,27 +307,10 @@
     border: 2px red solid;
   }
 
-  .group-amount-options{
-    margin: 2rem 0rem;
-    height: 100px;
-    width: 100%;
-  
-    border: 2px solid green;
-  }
-
-  .group-amount-option-text{
-    font-size: 50px;
-    font-weight: bold;
-  }
-
-  #third-amount-option{
-    background-color: var(--third-option-color);
-  }
-
   .donate-thank-you-panel{
     @apply
       flex
-      py-[1rem]
+      py-[3rem]
       justify-end
     ;
     grid-column: 1 / span 2;
@@ -228,11 +320,96 @@
     /* background-color: rgba(222, 220, 186, 1); */
   }
 
-  #group-thankyou-subsection{
-    display: grid;
+  #group-thank-you-subsection{
+    display: block;
     width: 100%;
     padding: 1rem 0px;
     border: 2px solid blue;
+  }
+
+  #amount-manual-wrapper{
+    margin-top: 2rem;
+    justify-content: center;
+    display: block;
+    text-align: center;
+    width: 100%;
+    height: 100px;
+    box-sizing: border-box;
+
+    /* border: 4px solid blue; */
+  }
+
+  #amount-manual-field{
+    text-align: center;
+    font-size: 3rem;
+    font-weight: bold;
+    font-family: 'Times New Roman';
+    height: 4rem;
+    border: 0px;
+    width: 80%;
+    border-bottom: 3px solid black;
+    background-color: rgba(0,0,0,0);
+  }
+
+  #amount-manual-field-text{
+    margin-top: 0.4rem;
+    font-size: 1.7rem;
+    font-weight: bold;
+  }
+
+  .amount-options-wrapper{
+    margin-top: 1rem;
+    justify-content: center;
+    text-align: center;
+    width: 100%;
+    box-sizing: border-box;
+
+    border: 4px solid purple;
+  }
+
+  .group-amount-options{
+    @apply
+      flex /* vertical center */
+      flex-col
+      justify-center
+    ;
+    cursor: pointer;
+    margin: 2.6rem 0rem;
+    height: 120px;
+    width: 100%;
+    box-shadow: 0;
+    transition: box-shadow 100ms;
+    border-radius: 10px;
+    box-sizing: border-box;
+    border: 2px solid green;
+  }
+
+  .group-amount-options:hover{
+    box-shadow: 0 0 4px black;
+  }
+
+  .group-amount-option-text{
+    font-size: 50px;
+    font-weight: bold;
+   
+    /* block text selection across browsers */
+    -moz-user-select: none;
+    -ktml-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  #first-amount-option{
+    background-color: var(--first-option-color); 
+  }
+
+  #second-amount-option{
+    background-color: var(--second-option-color);
+  }
+
+  #third-amount-option{
+    background-color: var(--third-option-color);
   }
 
   /* ---------------- Donate Form Section ---------------- */
@@ -252,7 +429,7 @@
     @apply
       flex
       justify-center
-      py-[3rem]
+      py-[2rem]
     ;
     width: 100%;
     border: 2px solid green;
@@ -421,11 +598,19 @@
   }
 
   #region-country-field{
+    background-color: white;
     border-radius: 10px 0px 0px 10px;
     grid-column: col 1 / span 1;
+    border: 2px solid black;
+  }
+
+  #region-country-field:focus{
+    outline: none; 
+   /* use shadow black in future */
   }
 
   #region-postal-code-field{
+    text-transform: uppercase;
     border-radius: 0px 10px 10px 0px;
     grid-column: col 2 / span 1;
   }
