1ffbe2461ebe7945fd295c45bd044bd3080a24e6
JoJo <l19priv@gmail.com>
2022-12-19 21:51:05 -0500

    goals cont'd

--- 02_Mignonne_Charity/src/routes/goals/+page.svelte
+++ 02_Mignonne_Charity/src/routes/goals/+page.svelte
@@ -35,7 +35,7 @@
           </div>
           <div class="panel-1-group-illustration-wrapper" id="panel-1-illustration-2-wrapper"> 
             <img 
-							src="https://dieupeut-bucket.us-east-1.linodeobjects.com/narrow_smiling_young.png" 
+							src="https://dieupeut-bucket.us-east-1.linodeobjects.com/narrow_old.png" 
 							class="panel-1-group-illustrations" id="panel-1-illustration-2" 
 						/> 
           </div>
@@ -51,6 +51,35 @@
     </div>
     <div class="group-panel-outer-wrapper" id="panel-2-outer-wrapper">
       <div class="group-panel-inner-wrapper" id="panel-2-inner-wrapper">
+
+				<div id="panel-2-title-wrapper">
+					<h1 id="panel-2-title-text">
+						Example Title For Panel Two
+					</h1>
+				</div>
+				<div id="panel-2-text-wrapper">
+					<div id="panel-2-text">
+							{ipsum_text}
+					</div>
+				</div>
+				<div id="panel-2-video-upper-wrapper">	
+					<div id="panel-2-video-inner-wrapper">	
+						<video
+							id="panel-2-video"
+							autoplay
+							muted
+							loop
+							width=100%
+							height=auto
+						>
+							<source 
+								src="https://dieupeut-bucket.us-east-1.linodeobjects.com/video_goal_EDIT.mp4"
+								type="video/mp4" 
+							/>
+						</video>
+					</div>	
+				</div>	
+
       </div>
     </div>
 
@@ -157,9 +186,12 @@
 	}
 
 	.panel-1-group-illustration-wrapper{
-		background-color: rgba(255,255,255,1);
+		/* background-color: rgba(255,255,255,1); */
+		/* background-color: rgba(204, 232, 237,1); */
+		background-color: rgba(218, 241, 245,1);
 		box-shadow: 0 0 3px black;
 		border-radius: 5px;
+		overflow: hidden;
 	}
 
 	#panel-1-illustration-1-wrapper{
@@ -176,11 +208,7 @@
 	}
 
 	#panel-1-illustration-2-wrapper{
-		@apply
-			flex
-			flex-col
-			justify-center
-		;
+		padding-top: 2rem;
 		grid-column: col 13 / span 7; 
 		grid-row: row 5 / span 11; 
 		width: 100%;	
@@ -214,5 +242,65 @@
 	}
 
   /* --------------- Panel 2 --------------- */
+  #panel-2-inner-wrapper{
+    display: grid;
+    grid-template-columns: repeat(20, [col] 5%);
+    grid-template-rows: repeat(20, [row] 5%);
+  }
+	
+	#panel-2-title-wrapper{
+    @apply
+      flex
+      flex-col
+      justify-center
+    ;
+    grid-column: col 1 / span 20;
+    grid-row: row 2 / span 2;
+      
+    width: 100%; 
+    box-sizing: border-box;
+    border: 1px solid green;
+	}
+
+  #panel-2-title-text{
+    width: 100%;
+    text-align: center;
+  }
+
+	#panel-2-text-wrapper{
+		grid-column: col 1 / span 20;
+		grid-row: row 5 / span 7;
+		border: 2px solid red;
+	}
+
+	#panel-2-video-upper-wrapper{
+		@apply
+			flex
+			justify-center
+		;
+		grid-column: col 1 / span 20;
+		grid-row: row 12 / span 8;
+		border: 2px solid blue;
+	}
+	
+	#panel-2-text{
+    padding: 1.5rem;
+		font-size: 1.5rem;
+	}
+
+	#panel-2-video-inner-wrapper{
+		width: 65%;		
+		height: 100%;		
+		overflow: hidden;
+		
+		border-radius: 10px;
+		box-sizing: border-box;
+		border: 2px solid green;
+	}
+
+	#panel-2-video{
+		box-sizing: border-box;
+		object-position: 0 -2.8rem;
+	}	
     
 </style>
